4af450ae72754d2f9dad1d44bddf9583fb0c7089
Darryl Kilzer <darrylkilzer@gmail.com>
2020-02-06 10:10:50 -0700

    init

---
+++ app/utils.js
@@ -0,0 +1,10 @@
+export const generateId = function () {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < 10; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+};
